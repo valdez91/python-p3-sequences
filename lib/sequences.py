5192efc3ace990be8497ca7d2456c8f8be1c2b83
Victor Kemboi <vickemtoo97@gmail.com>
2024-05-24 10:03:22 +0300

    sequencing

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,11 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib_sequence=[]
+    if length >0:
+        fib_sequence.append(0)
+    if length >=2:
+        fib_sequence.append(1)
+        for i in range(2,length):
+            fib_sequence.append(fib_sequence[-1] + fib_sequence[-2])
+    print(fib_sequence)
